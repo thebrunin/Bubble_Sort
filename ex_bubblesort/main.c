ae9668d9206124487f409de33bdd8b0aea3a7b67
Bruno Ribeiro <bruno.marshall22@gmail.com>
2022-07-28 10:17:17 -0300

    upgrade 1

--- ex_bubblesort/main.c
+++ ex_bubblesort/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void BubbleSort(int vet[], int tam);
+
 int main()
 {
     int vet[] = {9,2,8,4,5,6,1};
     int tam = sizeof(vet)/sizeof(int);
+    BubbleSort(vet, tam);
+}
+void BubbleSort(int vet[], int tam)
+{
     int aux, i, j;
-
-
     for(j=0;j<tam;j++){
         for(i=0;i<tam;i++){
 
@@ -16,9 +20,7 @@ int main()
             vet[i]=vet[i-1];
             vet[i-1]=aux;
             }
-            //printf("%d: %d \n", i, vet[i]);
         }
-        //printf("%d: %d \n", j, vet[j]);
     }
     for(int l=0;l<tam;l++){
         printf("%d: %d \n", l, vet[l]);
